5f283f72e2d93f3cedf568ed2558aabde11c2d73
dojinyou <djyou128@gmail.com>
2022-02-18 16:16:25 +0900

    Chore: nestjs config package 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { ContractModule } from './contract/contract.module';
 import { UserModule } from './user/user.module';
 import { AudioModule } from './audio/audio.module';
 import { BloomfilterModule } from './bloomfilter/bloomfilter.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://test:<password>@cluster0.rkbsz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
     IpfsModule,
     ContractModule,
     UserModule,
